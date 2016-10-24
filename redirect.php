ac6c843bd4330a51b762e9088842eba740908928
flokosiol <git@flokosiol.de>
2016-10-24 14:12:52 +0200

    add redirect template

--- redirect.php
+++ redirect.php
@@ -3,6 +3,9 @@
 // register blueprints
 $kirby->set('blueprint', 'redirect', __DIR__ . DS . 'blueprints' . DS . 'redirect.yml');
 
+// register templates
+$kirby->set('template', 'redirect', __DIR__ . DS . 'templates' . DS . 'redirect.php');
+
 // register models
 require(__DIR__ . DS . 'models' . DS . 'redirect.php');
 $kirby->set('page::model', 'redirect', 'RedirectPage');
